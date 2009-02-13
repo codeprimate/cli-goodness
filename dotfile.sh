59943db2ded753241a5f8ba45108cef2a1ffbf20
Russ Sherman <bhedana@gmail.com>
2009-02-13 13:46:29 -0600

    update readmes

--- dotfile.sh
+++ dotfile.sh
@@ -1,7 +1,6 @@
 source $CLI_GOODNESS/clig_env.sh
 source $CLI_GOODNESS/clig_aliases.sh
 source $CLI_GOODNESS/clig_alias_bash.sh
-source $CLI_GOODNESS/clig_alias_rails.sh
 source $CLI_GOODNESS/clig_alias_httpd.sh
 source $CLI_GOODNESS/clig_alias_httpd.sh
 
