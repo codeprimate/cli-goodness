20de5ba5fb0d094843ad26c0297743b994353c03
Russ Sherman <bhedana@gmail.com>
2009-02-13 13:03:57 -0600

    added rails-goodness

--- dotfile.sh
+++ dotfile.sh
@@ -11,5 +11,9 @@ source $GIT_GOODNESS/dotfile.sh
 export PROJ_GOODNESS="$CLI_GOODNESS/proj-goodness"
 source $PROJ_GOODNESS/dotfile.sh
 
+export RAILS_GOODNESS="$CLI_GOODNESS/rails-goodness"
+source $RAILS_GOODNESS/dotfile.sh
+
 export TWIT_GOODNESS="$CLI_GOODNESS/twit-goodness"
 source $TWIT_GOODNESS/dotfile.sh
+
---
+++ rails-goodness/dotfile.sh
@@ -0,0 +1,4 @@
+export PATH="$RAILS_GOODNESS:$PATH"
+source $RAILS_GOODNESS/railsg_env.sh
+source $RAILS_GOODNESS/railsg_env.sh
+
